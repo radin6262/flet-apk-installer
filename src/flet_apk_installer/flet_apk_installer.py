94b0bfdd6ca22d58e0d3904c4b3b52a2b0efd675
radin6262 <radindavari054@gmail.com>
2026-08-04 00:46:11 +0330

    Update flet_apk_installer.py

--- src/flet_apk_installer/flet_apk_installer.py
+++ src/flet_apk_installer/flet_apk_installer.py
@@ -8,8 +8,6 @@ class FletApkInstaller(ft.LayoutControl):
     Flet control for installing APK files on Android.
     """
 
-    path: str = ft.string_attr("path")
-
     def __init__(
         self,
         path: Optional[str] = None,
@@ -19,3 +17,14 @@ class FletApkInstaller(ft.LayoutControl):
 
         if path is not None:
             self.path = path
+
+    @property
+    def path(self) -> str:
+        return self._get_attr("path", "") or ""
+
+    @path.setter
+    def path(self, value: str):
+        self._set_attr("path", value)
+
+    def install(self):
+        self._dispatch_event("install", {})
